171f827b2df7b6e494527bdc466e555d9c421fdf
William Orr <81045133+WilliamJOrrIV@users.noreply.github.com>
2021-03-23 03:30:57 -0400

    Add files via upload

---
+++ Movie Rentals'/Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.VideosRentedTB = New System.Windows.Forms.TextBox()
+        Me.PriceTextBox = New System.Windows.Forms.TextBox()
+        Me.MemberNumberTB = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DiscountedPriceTextBox = New System.Windows.Forms.TextBox()
+        Me.DiscountTextBox = New System.Windows.Forms.TextBox()
+        Me.ExtendedPriceTextBox = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TotalCustomersTB = New System.Windows.Forms.TextBox()
+        Me.TotalVideosRentedTB = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.VideosRentedTB)
+        Me.GroupBox1.Controls.Add(Me.PriceTextBox)
+        Me.GroupBox1.Controls.Add(Me.MemberNumberTB)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 66)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(366, 125)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'VideosRentedTB
+        '
+        Me.VideosRentedTB.Location = New System.Drawing.Point(142, 22)
+        Me.VideosRentedTB.Name = "VideosRentedTB"
+        Me.VideosRentedTB.Size = New System.Drawing.Size(100, 22)
+        Me.VideosRentedTB.TabIndex = 5
+        '
+        'PriceTextBox
+        '
+        Me.PriceTextBox.Location = New System.Drawing.Point(142, 78)
+        Me.PriceTextBox.Name = "PriceTextBox"
+        Me.PriceTextBox.ReadOnly = True
+        Me.PriceTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.PriceTextBox.TabIndex = 4
+        Me.PriceTextBox.Text = "3.99"
+        '
+        'MemberNumberTB
+        '
+        Me.MemberNumberTB.Location = New System.Drawing.Point(142, 50)
+        Me.MemberNumberTB.Name = "MemberNumberTB"
+        Me.MemberNumberTB.Size = New System.Drawing.Size(100, 22)
+        Me.MemberNumberTB.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Price"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Member Number"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Videos Rented"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label4.Location = New System.Drawing.Point(121, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(133, 25)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Video Rentals"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DiscountedPriceTextBox)
+        Me.GroupBox2.Controls.Add(Me.DiscountTextBox)
+        Me.GroupBox2.Controls.Add(Me.ExtendedPriceTextBox)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 218)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(366, 108)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        '
+        'DiscountedPriceTextBox
+        '
+        Me.DiscountedPriceTextBox.Location = New System.Drawing.Point(260, 80)
+        Me.DiscountedPriceTextBox.Name = "DiscountedPriceTextBox"
+        Me.DiscountedPriceTextBox.ReadOnly = True
+        Me.DiscountedPriceTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.DiscountedPriceTextBox.TabIndex = 8
+        '
+        'DiscountTextBox
+        '
+        Me.DiscountTextBox.Location = New System.Drawing.Point(260, 47)
+        Me.DiscountTextBox.Name = "DiscountTextBox"
+        Me.DiscountTextBox.ReadOnly = True
+        Me.DiscountTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.DiscountTextBox.TabIndex = 7
+        '
+        'ExtendedPriceTextBox
+        '
+        Me.ExtendedPriceTextBox.Location = New System.Drawing.Point(260, 18)
+        Me.ExtendedPriceTextBox.Name = "ExtendedPriceTextBox"
+        Me.ExtendedPriceTextBox.ReadOnly = True
+        Me.ExtendedPriceTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.ExtendedPriceTextBox.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(139, 78)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(115, 17)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Discounted Price"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(139, 47)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 17)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "20% Discount"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(139, 18)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 17)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Extended Price"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TotalCustomersTB)
+        Me.GroupBox3.Controls.Add(Me.TotalVideosRentedTB)
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.Label8)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 347)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(366, 80)
+        Me.GroupBox3.TabIndex = 3
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Summary"
+        '
+        'TotalCustomersTB
+        '
+        Me.TotalCustomersTB.Location = New System.Drawing.Point(163, 55)
+        Me.TotalCustomersTB.Name = "TotalCustomersTB"
+        Me.TotalCustomersTB.ReadOnly = True
+        Me.TotalCustomersTB.Size = New System.Drawing.Size(100, 22)
+        Me.TotalCustomersTB.TabIndex = 8
+        '
+        'TotalVideosRentedTB
+        '
+        Me.TotalVideosRentedTB.Location = New System.Drawing.Point(163, 27)
+        Me.TotalVideosRentedTB.Name = "TotalVideosRentedTB"
+        Me.TotalVideosRentedTB.ReadOnly = True
+        Me.TotalVideosRentedTB.Size = New System.Drawing.Size(100, 22)
+        Me.TotalVideosRentedTB.TabIndex = 7
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(7, 60)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(111, 17)
+        Me.Label9.TabIndex = 5
+        Me.Label9.Text = "Total Customers"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(7, 30)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(137, 17)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Total Videos Rented"
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(12, 451)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(108, 23)
+        Me.CalculateButton.TabIndex = 4
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(154, 451)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(103, 23)
+        Me.ClearButton.TabIndex = 5
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(288, 451)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(90, 23)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(390, 486)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "form1"
+        Me.Text = "Bills Video Rentals"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents VideosRentedTB As TextBox
+    Friend WithEvents PriceTextBox As TextBox
+    Friend WithEvents MemberNumberTB As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents DiscountedPriceTextBox As TextBox
+    Friend WithEvents DiscountTextBox As TextBox
+    Friend WithEvents ExtendedPriceTextBox As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TotalCustomersTB As TextBox
+    Friend WithEvents TotalVideosRentedTB As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents CalculateButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ Movie Rentals'/Form1.vb
@@ -0,0 +1,83 @@
+﻿' Project : Homework 1
+'Author: William Orr
+'Description: Uses Variables and computations to calculate discounts and number of videos rented
+
+Public Class form1
+    Const DiscountPercentage As Decimal = 0.2
+    Dim TotalVideosRented As Integer
+    Dim CustomerCount As Integer = 0
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        Dim Quanity As Integer
+        Dim Price As Decimal
+        Dim ExtendedPrice, DiscountPrice, FinalPrice As Decimal
+
+        Try
+            Quanity = Integer.Parse(VideosRentedTB.Text)
+
+            Try
+                Price = Decimal.Parse(PriceTextBox.Text)
+
+                ExtendedPrice = Quanity * Price
+                ExtendedPriceTextBox.Text = ExtendedPrice.ToString("c")
+
+                ''Discount Calculation
+                DiscountPrice = ExtendedPrice * DiscountPercentage
+                DiscountTextBox.Text = DiscountPrice.ToString("c")
+
+                ''Calculate Final Price
+                FinalPrice = ExtendedPrice - DiscountPrice
+                DiscountedPriceTextBox.Text = FinalPrice.ToString("c")
+
+                TotalVideosRented += Quanity
+                CustomerCount = CustomerCount + 1
+
+                TotalVideosRentedTB.Text = TotalVideosRented.ToString
+                TotalCustomersTB.Text = CustomerCount.ToString
+
+
+            Catch ex As FormatException
+                MessageBox.Show("Can not be empty",
+                           "Data Entry Error",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Asterisk)
+                MemberNumberTB.Focus()
+                MemberNumberTB.SelectAll()
+
+            End Try
+
+        Catch ex As FormatException
+            MessageBox.Show("Can not be empty",
+                            "Data Entry Error",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Asterisk)
+            VideosRentedTB.Focus()
+            VideosRentedTB.SelectAll()
+
+        End Try
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TotalDiscountTextBox_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        VideosRentedTB.Text = String.Empty
+        MemberNumberTB.Text = String.Empty
+
+        VideosRentedTB.Focus()
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
